98f174f5a3bd4caaeb9352e6f902ab2e4b6d1548
Mohammed Nokri <nokri.mohammed@gmail.com>
2018-11-29 11:14:16 +0000

    Added Get req userlList

--- router.js
+++ router.js
@@ -3,8 +3,11 @@ const Authentication = require('./controllers/authentication');
 const passportConfig = require('./services/passport');
 const passport = require('passport');
 
-const requireAuth = passport.authenticate('jwt', { session : false });
+// Authenticating requests is as simple as calling passport.authenticate() and specifying which strategy to employ.
+const requireAuth = passport.authenticate('jwt', { session : false }); 
 const requireSignin = passport.authenticate('local', { session: false });
+
+const User = require('./models/user');
 
 
 module.exports = function (app) {
@@ -12,8 +15,19 @@ module.exports = function (app) {
     app.get('/', requireAuth, function(req, res){
         res.send({ hi: 'there' });
     });
+
+    app.get('/userList', requireAuth, function (req, res) {
+        User.find({}, function (err, users) {
+            var userMap = {};
+
+            users.forEach(function (user) {
+                userMap[user._id] = user;
+            });
+
+            res.send(userMap);
+        });
+    });
+
     app.post('/signin', requireSignin, Authentication.signin )
     app.post('/signup', Authentication.signup);
-
-    
 }
